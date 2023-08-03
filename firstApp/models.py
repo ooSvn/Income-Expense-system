0583d5b5e3adc7164653d3638c4f70da81679593
Ali Morajjabi <ali.morajjabi@ut.ac.ir>
2023-08-03 13:25:29 +0330

    fitst expense added

--- firstApp/models.py
+++ firstApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Expense(models.Model):
+    amount = models.fields.BigIntegerField()
+    date = models.fields.TimeField()
+    text = models.fields.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
